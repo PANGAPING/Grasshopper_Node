bd16b1c02aedf2aff7f303ff074a6a7290208d8a
PANGAPING <39149430+PANGAPING@users.noreply.github.com>
2019-05-08 08:37:31 +0800

    Update grouped_group_objects.cs

--- May 19/grouped_group_objects.cs
+++ May 19/grouped_group_objects.cs
@@ -1,9 +1,22 @@
-  private void RunScript(ref object A)
+/// <summary>
+/// This component will return the grouped reference object which you have grouped in rhino.
+/// Assume your groups have just one layer.
+/// </summary>
+
+
+  private void RunScript(List<Guid> geometries, ref object grouped_geometries)
   {
+    //The inputs need to be reference objects  meaning it is picked by hand and stored in some parameters battery.
+    //So it can be convert into Guid object.
+    List<RhinoObject> objs = new List<RhinoObject>();
 
     var map = new Dictionary<int,List<Guid>>();
 
-    foreach(var obj in RhinoDoc.ActiveDoc.Objects){
+
+    foreach (Guid guid in geometries){
+      // Only RhinoObject class has GetGroupList method, so we transfer ObjRef class to RhinoObject class. 
+      //Using Object method.
+      RhinoObject obj = new ObjRef(guid).Object();
       if (obj.GetGroupList() != null){
         if(map.ContainsKey(obj.GetGroupList()[0]) == false){
           map.Add(obj.GetGroupList()[0], new List<Guid>());
@@ -12,21 +25,14 @@
       }
     }
 
+
+
+
     var tree = new DataTree<Guid>();
 
     foreach (var groupIndex in map.Keys){
       tree.AddRange(map[groupIndex], new GH_Path(groupIndex));
     }
 
-    A = tree;
-
-
-
-    //var tree = new DataTree <Guid> ();
-    //foreach( var g in groups ) {
-    //var something = g.Select(id => id);
-    //tree.AddRange(g.Select(id => id), new GH_Path (g.Key));
-    //}
-    //A = groups;
-    //A = tree;
+    grouped_geometries = tree;
   }
